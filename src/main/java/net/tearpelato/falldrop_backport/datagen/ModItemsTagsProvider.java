e64ff241ebb4c83f597856f77e4a7b53fb92a624
TearPelato <165576102+TearPelato@users.noreply.github.com>
2026-08-09 20:05:41 +0200

    26.2 Fix

--- src/main/java/net/tearpelato/falldrop_backport/datagen/ModItemsTagsProvider.java
+++ src/main/java/net/tearpelato/falldrop_backport/datagen/ModItemsTagsProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.ItemTags;
+import net.tearpelato.falldrop_backport.Constants;
 import net.tearpelato.falldrop_backport.init.ModBlocks;
 import net.tearpelato.falldrop_backport.init.ModTags;
 
@@ -17,55 +18,55 @@ public class ModItemsTagsProvider extends FabricTagsProvider.ItemTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
 
-        valueLookupBuilder(ModTags.Items.POPLAR_LOGS)
-                .add(ModBlocks.POPLAR_LOG.asItem())
-                .add(ModBlocks.POPLAR_WOOD.asItem())
-                .add(ModBlocks.STRIPPED_POPLAR_LOG.asItem())
-                .add(ModBlocks.STRIPPED_POPLAR_WOOD.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_TOOL_MATERIALS).add(ModBlocks.POPLAR_PLANKS.asItem());
-        valueLookupBuilder(ItemTags.PLANKS).add(ModBlocks.POPLAR_PLANKS.asItem());
+        tag(ModTags.Items.POPLAR_LOGS)
+                .add(Constants.getItemKey(ModBlocks.POPLAR_LOG.asItem()))
+                .add(Constants.getItemKey(ModBlocks.POPLAR_WOOD.asItem()))
+                .add(Constants.getItemKey(ModBlocks.STRIPPED_POPLAR_LOG.asItem()))
+                .add(Constants.getItemKey(ModBlocks.STRIPPED_POPLAR_WOOD.asItem()));
+        tag(ItemTags.WOODEN_TOOL_MATERIALS).add(Constants.getItemKey(ModBlocks.POPLAR_PLANKS.asItem()));
+        tag(ItemTags.PLANKS).add(Constants.getItemKey(ModBlocks.POPLAR_PLANKS.asItem()));
 
-        valueLookupBuilder(ItemTags.WOODEN_BUTTONS).add(ModBlocks.POPLAR_BUTTON.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_DOORS).add(ModBlocks.POPLAR_DOOR.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_FENCES).add(ModBlocks.POPLAR_FENCE.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_SLABS).add(ModBlocks.POPLAR_SLAB.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_PRESSURE_PLATES).add(ModBlocks.POPLAR_PRESSURE_PLATE.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_STAIRS).add(ModBlocks.POPLAR_STAIRS.asItem());
-        valueLookupBuilder(ItemTags.WOODEN_TRAPDOORS).add(ModBlocks.POPLAR_TRAPDOOR.asItem());
+        tag(ItemTags.WOODEN_BUTTONS).add(Constants.getItemKey(ModBlocks.POPLAR_BUTTON.asItem()));
+        tag(ItemTags.WOODEN_DOORS).add(Constants.getItemKey(ModBlocks.POPLAR_DOOR.asItem()));
+        tag(ItemTags.WOODEN_FENCES).add(Constants.getItemKey(ModBlocks.POPLAR_FENCE.asItem()));
+        tag(ItemTags.WOODEN_SLABS).add(Constants.getItemKey(ModBlocks.POPLAR_SLAB.asItem()));
+        tag(ItemTags.WOODEN_PRESSURE_PLATES).add(Constants.getItemKey(ModBlocks.POPLAR_PRESSURE_PLATE.asItem()));
+        tag(ItemTags.WOODEN_STAIRS).add(Constants.getItemKey(ModBlocks.POPLAR_STAIRS.asItem()));
+        tag(ItemTags.WOODEN_TRAPDOORS).add(Constants.getItemKey(ModBlocks.POPLAR_TRAPDOOR.asItem()));
 
-        valueLookupBuilder(ModTags.Items.WOOL_SET)
-                .add(ModBlocks.WHITE_WOOL_STAIRS.asItem())
-                .add(ModBlocks.LIGHT_GRAY_WOOL_STAIRS.asItem())
-                .add(ModBlocks.GRAY_WOOL_STAIRS.asItem())
-                .add(ModBlocks.BLACK_WOOL_STAIRS.asItem())
-                .add(ModBlocks.BROWN_WOOL_STAIRS.asItem())
-                .add(ModBlocks.RED_WOOL_STAIRS.asItem())
-                .add(ModBlocks.ORANGE_WOOL_STAIRS.asItem())
-                .add(ModBlocks.YELLOW_WOOL_STAIRS.asItem())
-                .add(ModBlocks.LIME_WOOL_STAIRS.asItem())
-                .add(ModBlocks.GREEN_WOOL_STAIRS.asItem())
-                .add(ModBlocks.CYAN_WOOL_STAIRS.asItem())
-                .add(ModBlocks.LIGHT_BLUE_WOOL_STAIRS.asItem())
-                .add(ModBlocks.BLUE_WOOL_STAIRS.asItem())
-                .add(ModBlocks.PURPLE_WOOL_STAIRS.asItem())
-                .add(ModBlocks.MAGENTA_WOOL_STAIRS.asItem())
-                .add(ModBlocks.PINK_WOOL_STAIRS.asItem())
-                .add(ModBlocks.WHITE_WOOL_SLAB.asItem())
-                .add(ModBlocks.LIGHT_GRAY_WOOL_SLAB.asItem())
-                .add(ModBlocks.GRAY_WOOL_SLAB.asItem())
-                .add(ModBlocks.BLACK_WOOL_SLAB.asItem())
-                .add(ModBlocks.BROWN_WOOL_SLAB.asItem())
-                .add(ModBlocks.RED_WOOL_SLAB.asItem())
-                .add(ModBlocks.ORANGE_WOOL_SLAB.asItem())
-                .add(ModBlocks.YELLOW_WOOL_SLAB.asItem())
-                .add(ModBlocks.LIME_WOOL_SLAB.asItem())
-                .add(ModBlocks.GREEN_WOOL_SLAB.asItem())
-                .add(ModBlocks.CYAN_WOOL_SLAB.asItem())
-                .add(ModBlocks.LIGHT_BLUE_WOOL_SLAB.asItem())
-                .add(ModBlocks.BLUE_WOOL_SLAB.asItem())
-                .add(ModBlocks.PURPLE_WOOL_SLAB.asItem())
-                .add(ModBlocks.MAGENTA_WOOL_SLAB.asItem())
-                .add(ModBlocks.PINK_WOOL_SLAB.asItem());
+        tag(ModTags.Items.WOOL_SET)
+                .add(Constants.getItemKey(ModBlocks.WHITE_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIGHT_GRAY_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.GRAY_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BLACK_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BROWN_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.RED_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.ORANGE_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.YELLOW_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIME_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.GREEN_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.CYAN_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIGHT_BLUE_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BLUE_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.PURPLE_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.MAGENTA_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.PINK_WOOL_STAIRS.asItem()))
+                .add(Constants.getItemKey(ModBlocks.WHITE_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIGHT_GRAY_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.GRAY_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BLACK_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BROWN_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.RED_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.ORANGE_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.YELLOW_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIME_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.GREEN_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.CYAN_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.LIGHT_BLUE_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.BLUE_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.PURPLE_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.MAGENTA_WOOL_SLAB.asItem()))
+                .add(Constants.getItemKey(ModBlocks.PINK_WOOL_SLAB.asItem()));
 
     }
 }
